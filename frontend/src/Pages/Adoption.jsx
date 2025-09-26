9516647b785c4f33e4fc6817f572a06f4d536f42
DuleepaDabare <duleepadabare2003@gmail.com>
2025-09-26 22:52:47 +0530

    26/09/2025 this is Adoption UI

---
+++ frontend/src/Pages/Adoption.jsx
@@ -0,0 +1,17 @@
+
+import React,{useState} from "react";
+import Nav from '../Components/Nav/Nav'
+
+
+function Adoption() {
+  const [collapsed, setCollapsed] = useState(false);
+  return (
+    <div className="p-6">
+      {/* Sidebar */}
+      <Nav collapsed={collapsed} setCollapsed={setCollapsed} />
+      <h1>Adoption UI</h1>
+    </div>
+  )
+}
+
+export default Adoption

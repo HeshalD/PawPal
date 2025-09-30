7e989698afde5b7f6e657dfef39431b2eed0726d
vikisan2000 <vilanirathnyaka00@gmail.com>
2025-09-30 13:28:57 +0530

    30/09/2025 update vilani part

--- pawpal-frontend/src/App.js
+++ pawpal-frontend/src/App.js
@@ -1,32 +1,19 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './layout/Layout';
-import Dashboard from './pages/Dashboard';
-import HealthRecords from './pages/HealthRecords';
-import Doctors from './pages/Doctors';
-import Appointments from './pages/Appointments';
-import Chatbot from './pages/Chatbot';
-import { Pets, Donation, Sponsor, Adoption, Foster, Shop, Profile } from './pages/Placeholders';
+import AppointmentBooking from './pages/AppoinmentForm';
+import AppointmentDashboard from './pages/AppoinmentView';  
+import AdminAppointmentView from './pages/AdminAppoinmetView';
+import AdminHealthRec from './pages/AdminHealthRec';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/pets" element={<Pets />} />
-          <Route path="/donation" element={<Donation />} />
-          <Route path="/sponsor" element={<Sponsor />} />
-          <Route path="/adoption" element={<Adoption />} />
-          <Route path="/foster" element={<Foster />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/health-records" element={<HealthRecords />} />
-          <Route path="/doctors" element={<Doctors />} />
-          <Route path="/appointments" element={<Appointments />} />
-          <Route path="/chatbot" element={<Chatbot />} />
-        </Route>
+          
+          <Route path="/appoinmentBooking" element={<AppointmentBooking />} />
+          <Route path="/" element={<AdminHealthRec />} />
+
       </Routes>
     </BrowserRouter>
   );
